d143418c9780e4685b734121e4a946300fa3e31a
heike <hofmann@iastate.edu>
2022-09-15 12:28:31 -0500

    no more plyr

--- R/ancestors.R
+++ R/ancestors.R
@@ -1,70 +1,4 @@
-#' Scrape data for an academic record
-#'
-#' @param id integer number, the identifier of a mathematician in the Mathematics Genealogy Project
-#' @examples
-#' \dontrun{
-#' mathGen:::getNode(id = 15860) # John Wilder Tukey
-#' dh <- mathGen:::getNode(id = 7298) # David Hilbert
-#' }
-getNode <- function(id) {
-  url <- sprintf("http://genealogy.math.ndsu.nodak.edu/id.php?id=%s", id)
-  tabs <- httr::GET(url)
-  kids <- XML::readHTMLTable(rawToChar(tabs$content), stringsAsFactors = F)
 
-#  kids <- XML::readHTMLTable(url, stringsAsFactors=FALSE)
-  if (length(kids) > 0) kids <- kids[[1]]
-  else kids <- NULL
-
-  doc <- XML::htmlParse(tabs)
-  who <- XML::getNodeSet(doc, "//h2")
-  name <- trimws(XML::xmlValue(who[[1]]))
-  self <- data.frame(Name = name, mgID = id, stringsAsFactors = FALSE)
-
-  # more demographics
-  spans <- XML::getNodeSet(doc, "//span")
-  spanlist <- trimws(sapply(spans, XML::xmlValue))
-# browser()
-  # highly dependent on the format of Math Gen - very ugly
-  degree_University_Year <- spanlist[1]
-  self$School <- spanlist[2]
-  self$Thesistitle <- spanlist[4]
-  splits <- strsplit(degree_University_Year, split=" ")[[1]]
-  self$Degree <- splits[[1]]
-  self$Year <- as.numeric(splits[[length(splits)]])
-
-  # get all links and parse them for Math Gen IDs
-  hrefs <- XML::getNodeSet(doc, "//a")
-  refName <- sapply(hrefs, XML::xmlValue)
-  refIDs <- sapply(hrefs, XML::xmlGetAttr, name="href")
-  hits <- grep("id.php?id=", refIDs, fixed = TRUE)
-  refName <- refName[hits]
-  refIDs <- refIDs[hits]
-  refIDs <- gsub("id.php?id=", "", refIDs, fixed = TRUE)
-  selfID <- grep(id, refIDs, fixed = TRUE) # zero or one child leads to NULL
-  if (length(selfID) == 1) {
-    advID <- selfID-1
-    kidID <- selfID + 1
-  }
-  if (length(selfID) == 0) {
-    n <- length(refIDs)
-    if (!is.null(kids)) {
-      kidID <- n - nrow(kids) + 1
-      n <- kidID-1
-    }
-    advID <- n
-  }
-
-  if (advID <= 0) advisors <- NULL
-  else advisors <- data.frame(Name=as.character(refName[1:advID]),
-                              mgID=as.numeric(refIDs[1:advID]),
-                              stringsAsFactors = FALSE)
-  if (!is.null(kids)) {
-    kids$mgID <- as.numeric(refIDs[kidID:length(refIDs)])
-    kids$advisorName <- name
-    kids$advisorMGID <- id
-  }
-  list(self = self, advisor = advisors, advisees = kids)
-}
 #' Get a set number of ancestors based on name and Math Gen ID
 #'
 #' Starting from the given node, doctoral advisors are followed recursively for the specified number of steps. In case no advisor is known, the algorithm stops even if the number of steps is not yet reached.
@@ -89,7 +23,7 @@ getNode <- function(id) {
 #' @examples
 #' \dontrun{
 #' hw <- ancestry(id = 145799, steps = 4) # Hadley Wickham
-#' ancestry(id = 145799,  steps = 2, siblings = TRUE) # Hadley Wickham
+#' ancestry(id = 145799,  steps = 10, siblings = TRUE) # Hadley Wickham
 #' dh <- ancestry(id=7298,  steps = 5) # David Hilbert
 #'
 #' library(ggplot2)
